0ef5d6cf97df8e023bf6d5db65617d18c6e7a378
afif14 <mafifsolahudin@gmail.com>
2022-12-26 14:22:07 +0700

    slicing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import logo from './assets/icons/logo.svg'
 import './App.css';
+import jumbotron from './assets/images/jumbotron-img.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className='App'>
+          <div className='left-container basis-2/5'>
+              <img className='m-5' src={logo} alt='logo-icon' />
+              <img src={jumbotron} alt='jumbotron-image' />
+          </div>
+          <div className='right-container basis-3/5'>
+            <div>
+              <span>EN/USD<img src='./assets/icons/arrow-down.svg' alt='arrow-down'/></span>
+            </div>
+              <h2 className='text-[38px] text-white-base'>Sign up</h2>
+              <p className='text-[#777E91] text-xs'>Use Your OpenID to Sign up</p>
+              <div className='flex'>
+                  <span className='text-[#02C3BD]'>Email</span>
+                  <span>Phone Number</span>
+              </div>
+
+              <form className='text-white-base'>
+                  <div >
+                      <label htmlFor='emailInput'>Email</label>
+                      <input id='emailInput' type='email' name='email' />
+                  </div>
+                  <div>
+                      <label>Password</label>
+                      <input type='password' name='password' />
+                  </div>
+                  <div>
+                      <label>Confirm Password</label>
+                      <input type='password' name='confirmPassword' />
+                  </div>
+              </form>
+          </div>
+      </div>
+  )
 }
 
 export default App;
